51a3acbf099a7c3d1b573b9ade24fe283d253cbc
komezc <88783106+komezc@users.noreply.github.com>
2022-09-19 11:47:34 +0300

    ilk yukleme

---
+++ loopDemo/src/loopDemo-Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //for
+
+        for(int i=2;i<10;i+=2) {
+            System.out.println(i);
+        }
+
+        System.out.println("for Dongusu Bitti");
+        int i=2;
+        //While
+        while(i<10){
+            System.out.println(i);
+            i+=2;
+        }
+        System.out.println("while Dongusu Bitti");
+    }
+}
---
+++ variables/src/variables-Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ogrenciSayisi = 12;
+        String mesaj = "Ogrenci Sayisi : ";
+
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Ogrenci Sayisi : "+ ogrenciSayisi);
+        System.out.println("Ogrenci Sayisi : "+ ogrenciSayisi);
+        System.out.println("Ogrenci Sayisi : "+ ogrenciSayisi);
+    }
+}
